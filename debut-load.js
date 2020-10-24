a90fbeafc3acbdca538f307172fad0449f889a99
Ashish Manghani <57142359+ashishm1@users.noreply.github.com>
2020-10-24 15:49:44 +0530

    Update debut-load.js

--- debut-load.js
+++ debut-load.js
@@ -1,11 +1,12 @@
 try{
     const script2 = document.createElement('script');
-    script2.src = "https://code.jquery.com/jquery-3.5.1.min.js";
-    script2.type = "text/javascript";
-    console.log("From Jsdeliver")
-    script2.onreadystatechange = handlertag;
-    script2.onload = handlertag;
     document.getElementsByTagName('head')[0].appendChild(script2);
+    script2.onload = handlertag;
+    script2.onreadystatechange = handlertag;
+    script2.type = "text/javascript";
+    script2.src = "https://code.jquery.com/jquery-3.5.1.min.js";
+    console.log("From Jsdeliver")
+    
 }
 catch(err){
     console.log(err);
@@ -21,11 +22,11 @@ function handlertag() {
     try{
         console.log("This is debut theme api!!!!!!!")
         const lazyscript = document.createElement('script');
-        lazyscript.src = "https://cdn.shopify.com/s/files/1/0382/4185/files/ajaxinate.js?937";
-        lazyscript.type = "text/javascript";
-        lazyscript.onreadystatechange = addAjax;
-        lazyscript.onload = addAjax;
         document.getElementsByTagName('head')[0].appendChild(lazyscript);
+        lazyscript.onload = addAjax;
+        lazyscript.onreadystatechange = addAjax;
+        lazyscript.type = "text/javascript";
+        lazyscript.src = "https://cdn.shopify.com/s/files/1/0382/4185/files/ajaxinate.js?937";
         console.log("handlerTag completed");
     }
     catch(err){
@@ -36,7 +37,6 @@ function handlertag() {
 
 function addAjax(){
     console.log("In addAjax function")
-    $('body').append(`<script>window.addEventListener("load", function() { var endlessScroll = new Ajaxinate({container: "#LazyLoader-Loop", pagination: "#LazyLoader-Pagination"});});</script>`)
     if($('ul.grid').length > 0) {
         console.log("In ul grid")
         $('ul.grid').attr("id", "LazyLoader-Loop");
@@ -50,6 +50,8 @@ function addAjax(){
         console.log("In next arrow")
         $('a.btn--narrow').parent().attr("id", "LazyLoader-Pagination")
 }
+        $('body').append(`<script>window.addEventListener("load", function() { var endlessScroll = new Ajaxinate({container: "#LazyLoader-Loop", pagination: "#LazyLoader-Pagination"});});</script>`)
+
 }
 
 
