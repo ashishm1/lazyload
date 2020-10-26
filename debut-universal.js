5d1f75e75920252d437a0ede79ba8d006fcf42a8
Ashish Manghani <57142359+ashishm1@users.noreply.github.com>
2020-10-26 13:21:46 +0530

    Update debut-universal.js

--- debut-universal.js
+++ debut-universal.js
@@ -5,8 +5,6 @@ try{
     script2.onreadystatechange = handlertag;
     script2.type = "text/javascript";
     script2.src = "https://code.jquery.com/jquery-3.5.1.min.js";
-    console.log("From Jsdeliver")
-    
 }
 catch(err){
     console.log(err);
@@ -15,14 +13,12 @@ catch(err){
 
 function handlertag() {
     try{
-        console.log("This is debut theme api!!!!!!!")
         const lazyscript = document.createElement('script');
         document.getElementsByTagName('head')[0].appendChild(lazyscript);
         lazyscript.onload = addAjax;
         lazyscript.onreadystatechange = addAjax;
         lazyscript.type = "text/javascript";
         lazyscript.src = "https://cdn.jsdelivr.net/gh/ashishm1/lazyload@main/ajaxinate-debut.js";
-        console.log("handlerTag completed");
     }
     catch(err){
         console.log(err);
@@ -50,8 +46,8 @@ function addAjax(){
         }
     document.querySelectorAll('ul.pagination')[0].querySelectorAll('li')[1].innerText = "";
     document.querySelectorAll('ul.pagination')[0].querySelectorAll('li')[0].remove();
-    console.log(orientation)
-    $('main').append(`<script>var endlessScroll = new Ajaxinate({container: "#LazyLoader-Loop", pagination: "#LazyLoader-Pagination", orientation: orientation});document.addEventListener("DOMContentLoaded", function() { var endlessScroll = new Ajaxinate({container: "#LazyLoader-Loop", pagination: "#LazyLoader-Pagination", orientation: orientation});});</script>`)
+    
+    $('main').append(`<script>var endlessScroll = new Ajaxinate({container: "#LazyLoader-Loop", pagination: "#LazyLoader-Pagination", orientation: "${orientation}"});document.addEventListener("DOMContentLoaded", function() { var endlessScroll = new Ajaxinate({container: "#LazyLoader-Loop", pagination: "#LazyLoader-Pagination", orientation: "${orientation}"});});</script>`)
 
 
 }
